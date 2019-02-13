dffa51f09c47ee7dac3843002a2d7cc03a95e8ea
jwdavis <jeff@jwdavis.me>
2019-02-13 14:54:32 -0800

    production rollout
    - use env for app config
    - use inputs directory for videos
    - use outputs directory for outputs
    - update readme with directions

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
-# Demo: video to text, to translated text, to mp3
+# Universal Translation SErvice (UTS)Demo
+
+## Overview
+This demo is intended to show the "magic" you can achieve using GCP's ML APIs.
+
+It takes one or more input videos and does the following:
+
+1. Uses the Video Intelligence API to transcribe the spoken dialog
+2. Uses the Translation API to translate the dialog into a new language
+3. Uses the Speech-to-Text API to generate new spoken dialog
+
+This demo could be extended to then reconstruct the video with the new dialog (that's up to you)
 
 ## Setup
 1. Create or get access to a GCP project
-1. In the project, make sure the APIs are enabled for Cloud Text-to-Speech API, Cloud Translation API, Cloud Video Intelligence API
+1. In the project, make sure the APIs are enabled for
+	2. Cloud Text-to-Speech API, 
+	3. Cloud Translation API, 
+	4. Cloud Video Intelligence API
 1. Create a Service Account and download the key file to the root folder of this demo (yea, right next to this file is good)
-1. Give the Service Account Project Viewer permissions
-1. Edit the analyze.js file and set the projectId 
-1. Open a command prompt and change into the root of this project (I like to use VSCode, and open the terminal there so I can show the code while doing these other things)
-1. Run __*npm i*__ to rebuild the *node_modules* dependency folder
-1. In the command prompt set an environmental variable GOOGLE_APPLICATION_CREDENTIALS and point it at your downloaded keyfile. On a Mac it's __*export GOOGLE_APPLICATION_CREDENTIALS="roi-ml-video-to-text-whatever.json"*__
-1. Run __*npm start*__ to execute the code. 
+1. Assign the Project Viewer role to the service account within the project
+2. Set environment variables to configure the application
+	3. ```export DEVSHELL_PROJECT_ID=<your-project-name>``` (unnecessary if running in Cloud Shell)
+	4. ```export TARGET_LANGUAGE=<language-code>``` (see [https://cloud.google.com/translate/docs/languages]())
+	5. ```export VOICE=<voice-language-code>>``` (see [https://cloud.google.com/speech-to-text/docs/languages]())
+	6. ```export GOOGLE_APPLICATION_CREDENTIALS=<path-to-keyfile>```
+1. Copy any demo videos you like into the *inputs* directory
 
-After the run three files should appear. The video transcription file, the translation file, and the spoken translation mp3. All three files will have the same timestamp prefix.
+## Demo
+1. Open a command prompt and change into the root of this project (I like to use VSCode, and open the terminal there so I can show the code while doing these other things)
+1. Run ```npm i``` to rebuild the *node_modules* dependency folder
+1. Run ```npm start``` to execute the code. 
+
+After the run three files should be written for each video into the outputs directory. The video transcription file, the translation file, and the spoken translation mp3. 
+
+1. Show the students the transcript
+2. Show the students the translated text
+3. Play the new audio file for the students
+4. Discuss variations that would be interesting
--- analyze.js
+++ analyze.js
@@ -1,43 +1,61 @@
-
 /*
   This is a demonstration of some of the pre-trained GCP ML models. Specifically,
   it takes a short input video, extracts the audio track as text, saves
-  it as a file, runs a translation, safes that as a file, and 
+  it as a file, runs a translation, safes that as a file, and
   finally it creates a new audio track and saves it as an mp3.
 */
+
 // The project you're using. You may need to enable some of the ML
-// APIs to allow this to work, like: 
+// APIs to allow this to work, like:
 // Cloud Text-to-Speech API
 // Cloud Translation API
 // Cloud Video Intelligence API
-const projectId = 'roi-ml-video-to-text';
+const projectId = process.env.DEVSHELL_PROJECT_ID;
+
 //required libraries:
 //To extract the video's transcription:
 const videoIntelligence = require('@google-cloud/video-intelligence');
+
 //For easy access to the local file system:
 const fs = require('fs');
+
 //Some general utilities
 const util = require('util');
 //To do the translation:
+
 const {
   Translate
 } = require('@google-cloud/translate');
+
 //To create the new audio track:
 const textToSpeech = require('@google-cloud/text-to-speech');
-// The target language
-const targetLanguage = 'fr';
-//Input video file. This is local now, but it could just as easily
-//load out of a bucket:
-const inVideoFile = 'sample.mp4';
+
+//The target language for translation
+if (process.env.TARGET_LANGUAGE){
+  var targetLanguage = process.env.TARGET_LANGUAGE;
+} else{
+  var targetLanguage = 'fr';
+}
+
+//The target language for translation
+if (process.env.VOICE){
+  var voice = process.env.TARGET_LANGUAGE;
+} else{
+  var voice = 'en-US';
+}
 
 //This is the function doing all the work. It is called at the very
 //bottom of this file in a try catch
-async function analyzeVideoTranscription() {
-  console.log(`Let's go! Translating ${inVideoFile} into ${targetLanguage}`);
+async function analyzeVideoTranscription(filename, targetLanguage) {
+  var inVideoFile = `inputs/${filename}`;
+  var outputPrefix = `outputs/${filename}`;
+  console.log(`Starting job! Translating ${inVideoFile} into ${targetLanguage}`);
+
   // Creates the Video Intelligence client
   const videoIntelligenceClient =
     new videoIntelligence.VideoIntelligenceServiceClient();
     console.log("videoIntelligenceClient created");
+
   //options
   const videoContext = {
     speechTranscriptionConfig: {
@@ -45,13 +63,15 @@ async function analyzeVideoTranscription() {
       enableAutomaticPunctuation2: true,
     },
   };
+
   //Reads the local video file and base64 encodes it
   console.log("Base64 encoding local video file");
   const readFile = util.promisify(fs.readFile);
   const file = await readFile(inVideoFile);
   const inputContent = file.toString('base64');
 
-  console.log("Video file encoded");
+  console.log(`${filename} encoded`);
+
   //what we want it to do. If you wanted to load the file from
   //a bucket, you could simply use inputUri instead of content
   //and pass it a gs:// url
@@ -60,9 +80,9 @@ async function analyzeVideoTranscription() {
     features: ['SPEECH_TRANSCRIPTION'],
     videoContext: videoContext,
   };
-  console.log('Transcribing video...');
+  console.log(`Transcribing video ${filename}...`);
   const [operation] = await videoIntelligenceClient.annotateVideo(request);
-  
+
   const [operationResult] = await operation.promise();
 
   const alternative =
@@ -79,17 +99,16 @@ async function analyzeVideoTranscription() {
   // });
 
   const videoTranscript = alternative.transcript;
-  console.log('Transcription: ' + videoTranscript);
-  //Make up a file name that won't collide with other files:
-  const datePrefix = Date.now();
-  const oname = datePrefix + '_transcript.txt';
+  console.log(`${filename} transcription: ${videoTranscript}`);
+  const oname = outputPrefix + '_transcript.txt';
+
   //
   fs.writeFile(oname, videoTranscript, 'utf8', (err) => {
     if (err) throw err;
-    console.log('The video transcript file has been saved!');
+    console.log(`The video transcript file for ${filename} has been saved!`);
   });
 
-  console.log("Now doing the translation");
+  console.log(`Now doing the translation for ${filename}`);
 
   // Instantiates a translate client
   const translate = new Translate({
@@ -102,19 +121,21 @@ async function analyzeVideoTranscription() {
       textTranslation = results[0];
 
 
-      console.log(`Original text: ${videoTranscript}`);
-      console.log(`Translation: ${textTranslation}`);
-      console.log("Writing translation file");
-      //unique filename for translated text file
-      const transFileName = datePrefix + `_translated_to_${targetLanguage}.txt`;
+      console.log(`Original text for ${filename}: ${videoTranscript}`);
+      console.log(`Translation for ${filename}: ${textTranslation}`);
+      console.log(`Writing translation file for ${filename}`);
+
+      const transFileName = outputPrefix + `_translated_to_${targetLanguage}.txt`;
+
       //write the translated text
       fs.writeFile(transFileName, textTranslation, 'utf8', (err) => {
         if (err) throw err;
-        console.log('The video transcript file has been saved!');
+        console.log(`The video transcript file for ${filename} has been saved!`);
       });
     }).then(input => { //wait on all the translation file stuff to finish
 
-      console.log("Creating new audio file");
+      console.log(`Creating new audio file for ${filename}`);
+
       // Creates a client for text to speech
       const speechClient = new textToSpeech.TextToSpeechClient();
 
@@ -125,7 +146,7 @@ async function analyzeVideoTranscription() {
         },
         // Select the language and SSML Voice Gender (optional)
         voice: {
-          languageCode: 'fr-CA',
+          languageCode: voice,
           ssmlGender: 'MALE'
         },
         // Select the type of audio encoding
@@ -140,7 +161,8 @@ async function analyzeVideoTranscription() {
           console.error('synthesizeSpeech error:', err);
           return;
         }
-        const speechName = datePrefix + '_speech.mp3';
+        const speechName = outputPrefix + '_speech.mp3';
+
         // Write the binary audio content to a local file
         fs.writeFile(speechName, response.audioContent, 'binary', err => {
           if (err) {
@@ -156,13 +178,20 @@ async function analyzeVideoTranscription() {
     .catch(err => {
       console.error('ERROR:', err);
     });
-
-
 }
 
+fs.readdir('inputs', function(err, files){
+  if (err){
+    console.error("Could not list files in directory", err);
+  } else{
+    files.forEach(function(file, index){
+      try {
+        analyzeVideoTranscription(file, targetLanguage);
+      } catch (err) {
+        console.log("Blew up calling analyzeVideoTranscription(): " + err);
+      }
+    });
+  }
+});
 
-try {
-  analyzeVideoTranscription();
-} catch (err) {
-  console.log("Blew up calling analyzeVideoTranscription(): " + err);
-}
+
